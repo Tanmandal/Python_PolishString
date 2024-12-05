64776200170905e4f1c669a3ce0d748ba54b8504
Tanmay Mandal <93572359+Tanmandal@users.noreply.github.com>
2024-12-06 01:55:31 +0530

    Delete PolishString directory

--- PolishString/polishstring/__init__.py
+++
@@ -1,6 +0,0 @@
-from .polishstring import inf_prf
-from .polishstring import inf_pof
-from .polishstring import pof_prf
-from .polishstring import pof_inf
-from .polishstring import prf_pof
-from .polishstring import prf_inf
